88b8d2a75b339cd5c485b2b9dac5282711a03100
JAlvarezz18 <johanalvarezz237@gmail.com>
2023-04-02 13:19:30 -0500

    final choose

--- src/apis/HowApi.js
+++ src/apis/HowApi.js
@@ -41,4 +41,35 @@ const how = [
   },
 ];
 
-export { how };
+const choose = [
+  {
+    id: 1,
+    porcentaje: "1,5",
+    term: "20 days",
+    pricipal: "Ruturn",
+    color: "bg-[#ba40d0]",
+  },
+  {
+    id: 2,
+    porcentaje: "2",
+    term: "20 days",
+    pricipal: "Ruturn",
+    color: "bg-[#dc566f]",
+  },
+  {
+    id: 3,
+    porcentaje: "650",
+    term: "35 days",
+    pricipal: "Include",
+    color: "bg-[#db15a3]",
+  },
+  {
+    id: 4,
+    porcentaje: "1,500",
+    term: "55 days",
+    pricipal: "Include",
+    color: "bg-[#e26625]",
+  },
+];
+
+export { how, choose };
--- src/components/heroComponets/ChooseComponent.jsx
+++ src/components/heroComponets/ChooseComponent.jsx
@@ -1,82 +1,33 @@
-import React from "react";
+import React, { useState } from "react";
+import { choose } from "../../apis/HowApi";
 
 export const ChooseComponent = () => {
+  const [Choose, setChoose] = useState(choose);
+
   return (
     <div className="grid grid-cols-2">
       {/* Left */}
       <div className="grid grid-cols-2 place-content-between w-[32rem] gap-6">
-        <article className="bg-colorSecundario  h-44 p-4 flex flex-col justify-between rounded-lg shadow">
-          <div className="bg-gradient-to-r from-[#f05908] to-[#c54fc4] w-10 h-2  rounded-full "></div>
-          <p className="text-5xl font-semibold">
-            1,5 <span className="text-base font-light">%</span>
-          </p>
+        {Choose.map((item) => (
+          <article className="bg-colorSecundario  h-44 p-4 flex flex-col justify-between rounded-lg shadow">
+            <div className={`${item.color} w-10 h-2  rounded-full `}></div>
+            <p className="text-5xl font-semibold">
+              {item.porcentaje} <span className="text-base font-light">%</span>
+            </p>
 
-          <div className="font-light">
-            <div className="flex justify-between">
-              <p className="text-gray-400">TERM</p>
-              <p className="font-medium">20 days</p>
-            </div>
+            <div className="font-light">
+              <div className="flex justify-between">
+                <p className="text-gray-400">TERM</p>
+                <p className="font-medium">{item.term}</p>
+              </div>
 
-            <div className="flex justify-between">
-              <p className="text-gray-400">PRINCIPAL</p>
-              <p className="font-medium">20 days</p>
+              <div className="flex justify-between">
+                <p className="text-gray-400">PRINCIPAL</p>
+                <p className="font-medium">{item.pricipal}</p>
+              </div>
             </div>
-          </div>
-        </article>
-        <article className="bg-colorSecundario  h-44 p-4 flex flex-col justify-between rounded-lg shadow">
-          <div className="bg-gradient-to-r from-[#f05908] to-[#c54fc4] w-10 h-2  rounded-full "></div>
-          <p className="text-5xl font-semibold">
-            1,5 <span className="text-base font-light">%</span>
-          </p>
-
-          <div className="font-light">
-            <div className="flex justify-between">
-              <p className="text-gray-400">TERM</p>
-              <p className="font-medium">20 days</p>
-            </div>
-
-            <div className="flex justify-between">
-              <p className="text-gray-400">PRINCIPAL</p>
-              <p className="font-medium">20 days</p>
-            </div>
-          </div>
-        </article>
-        <article className="bg-colorSecundario  h-44 p-4 flex flex-col justify-between rounded-lg shadow">
-          <div className="bg-gradient-to-r from-[#f05908] to-[#c54fc4] w-10 h-2  rounded-full "></div>
-          <p className="text-5xl font-semibold">
-            1,5 <span className="text-base font-light">%</span>
-          </p>
-
-          <div className="font-light">
-            <div className="flex justify-between">
-              <p className="text-gray-400">TERM</p>
-              <p className="font-medium">20 days</p>
-            </div>
-
-            <div className="flex justify-between">
-              <p className="text-gray-400">PRINCIPAL</p>
-              <p className="font-medium">20 days</p>
-            </div>
-          </div>
-        </article>
-        <article className="bg-colorSecundario  h-44 p-4 flex flex-col justify-between rounded-lg shadow">
-          <div className="bg-gradient-to-r from-[#f05908] to-[#c54fc4] w-10 h-2  rounded-full "></div>
-          <p className="text-5xl font-semibold">
-            1,5 <span className="text-base font-light">%</span>
-          </p>
-
-          <div className="font-light">
-            <div className="flex justify-between">
-              <p className="text-gray-400">TERM</p>
-              <p className="font-medium">20 days</p>
-            </div>
-
-            <div className="flex justify-between">
-              <p className="text-gray-400">PRINCIPAL</p>
-              <p className="font-medium">20 days</p>
-            </div>
-          </div>
-        </article>
+          </article>
+        ))}
       </div>
 
       {/* Right */}
@@ -86,7 +37,7 @@ export const ChooseComponent = () => {
             <p className="text-colorSecundario text-md mb-2">NUMBERS</p>
             <h2 className="text-4xl">Choose Your Plane</h2>
           </div>
-          
+
           <div className="">
             <p className="text-gray-400">
               Lorem ipsum dolor sit amet consectetur adipisicing elit. Sed
@@ -94,9 +45,9 @@ export const ChooseComponent = () => {
               reiciendis asperiores sit eos, maxime aspernatur, tempora velit ex
               culpa ullam laborum, incidunt accusamus?
             </p>
-          
+
             <button className="bg-gradient-to-r from-[#f05908] to-[#c54fc4] px-6 py-2 rounded-full w-max mt-7">
-              Get Started
+              Read More
             </button>
           </div>
         </div>
