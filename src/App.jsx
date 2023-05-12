8d16d17afc07c900215dda9393d31ff4920c34c9
TJIGRIS <johanalvarezz237@gmail.com>
2023-05-11 20:06:55 -0500

    holi

--- src/App.jsx
+++ src/App.jsx
@@ -2,8 +2,8 @@ import React from "react";
 import "./index.css";
 
 import { Header } from "./pages/Header";
-import { Hero } from "./pages/Hero";
 import { Footer } from "./pages/Footer";
+import { Hero } from "./pages/Hero";
 
 function App() {
   return (
